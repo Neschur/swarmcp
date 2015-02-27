bb5a41d17ecbf98389f300818c48fdbca1165fae
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2015-02-27 12:38:18 +0300

    Login

--- app.rb
+++ app.rb
@@ -1,10 +1,56 @@
 require 'rubygems'
+require 'byebug'
 require 'sinatra'
+require 'json'
+
+class User
+  def self.login user, session
+    socket = get_socket
+    socket.puts({
+        command: 'login',
+        login: user[:name],
+        password: user[:password],
+        key: key(session),
+        host: 'localhost'
+      }.to_json)
+    response = socket.gets
+    socket.close
+    response.strip == 'true'
+  end
+
+  private
+  def self.get_socket
+    TCPSocket.new('localhost', 2626)
+  end
+
+  def self.key session
+    session[:session_id]
+  end
+end
 
 configure do
   enable :sessions
 end
 
+helpers do
+  def flash
+    return if !session[:error]
+    message = session[:error]
+    session[:error] = nil
+    "<div class='alert alert-danger'>#{message}</div>"
+  end
+end
+
 get '/' do
-  erb :login
+  session[:user] ? erb(:panel) : erb(:login)
+end
+
+post '/login' do
+  login = User.login(params[:user], session)
+  if !login
+    session[:error] = "Wrong username/password"
+  else
+    session[:user] = params[:user][:name]
+  end
+  redirect '/'
 end
