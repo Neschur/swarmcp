67e79c3b628e5ad9b796b39723a41dcaf65fdcdf
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2015-02-27 12:38:05 +0300

    Added sshserver

---
+++ sshserver/server.rb
@@ -0,0 +1,30 @@
+require 'socket'
+require 'json'
+load 'user.rb'
+
+server = TCPServer.open(2626)
+puts 'server started'
+loop do
+  begin
+    client = server.accept
+    request = JSON.parse(client.gets)
+    puts (request['password'] ? request.merge({'password' => '****'}) : request)
+
+    case request['command']
+    when 'login'
+      result = User.login(request['login'], request['password'], request['key'], request['host'])
+      client.puts result
+    when 'exec'
+      if User.login?(request['key'])
+        client.puts User.exec(request['key'], request['line'])
+      else
+        client.puts nil
+      end
+    else
+      puts 'undifined command'
+    end
+    client.close
+  rescue StandardError => e
+    puts 'ERROR ' + e.to_s
+  end
+end
---
+++ sshserver/user.rb
@@ -0,0 +1,47 @@
+require 'net/ssh'
+
+class User
+  EXPIRE_TIME = 3600
+  @@shells = {}
+
+  def self.login name, password, key, host
+    shell = User.get_shell(name, password, host)
+    @@shells[key] = {
+      shell: shell,
+      time: Time.now,
+    }
+    !!shell
+  end
+
+  def self.login? key
+    !!shell(key)
+  end
+
+  def self.exec key, command
+    shell(key).exec!(command)
+  end
+
+  private
+  def self.shell key
+    shell = @@shells[key]
+    return if !shell
+    if (Time.now - shell[:time] > EXPIRE_TIME)
+      @@shells[session[:session_id] + session[:_csrf_token]] = nil
+    else
+      shell[:shell]
+    end
+  end
+
+  def self.get_shell name, password, host
+    shell = nil
+    start_time = Time.new
+    login = Thread.new do
+      shell = Net::SSH.start(host, name, :password => password)
+    end
+    while(!(Time.now - start_time > 5 || shell))
+      sleep(0.1)
+    end
+    login.kill
+    shell
+  end
+end
